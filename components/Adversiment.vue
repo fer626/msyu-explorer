02c865e2a035e6a3973188a656d06b2027278fd1
Fernando León <fernando.leon92@gmail.com>
2022-03-30 17:25:52 +0000

    adversiemnt add but not work

---
+++ components/Adversiment.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dev-adversiment">
+    <h1>This app is under development</h1>
+    <p>There is a possibility that it has multiple failures or performance problems</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Adver-siment',
+  data: () => ({
+    close: false,
+    accept: false
+  })
+}
+</script>
+
+<style scoped>
+.dev-adversiment {
+  position: absolute;
+}
+</style>
