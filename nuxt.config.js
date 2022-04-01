2fb9fb5464a662fcab3983559506cbf43f994fd1
Fernando León <fernando.leon92@gmail.com>
2022-04-01 18:02:50 +0000

    plugin added

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,15 +25,13 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/tooltip.js' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   // components: true,
   components: [
-    {
-      path: '~/components',
-      pathPrefix: false
-    }
+    { path: '~/components', pathPrefix: false }
   ],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
