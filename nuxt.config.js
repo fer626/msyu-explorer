ff9994b001ce94f32416bb42a3b24096ad5f0b91
Fernando A. León <fernando.leon92@gmail.com>
2022-04-26 15:52:14 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: true,
+  ssr: false,
   dev: process.env.NODE_ENV !== 'production',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
